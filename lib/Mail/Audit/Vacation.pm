dc45844d0823a40b1bd5b755ec1e8407e6008c5a
Ricardo Signes <rjbs@cpan.org>
2007-06-14 22:30:24 +0000

    r31848@knight:  rjbs | 2007-06-14 18:29:51 -0400
     security fix w/r/t writing files to tmp without checking
     put _log crap into mime messages (Stupid autotype)
     allow reason arg to ignore
     document ->log
     r31849@knight:  rjbs | 2007-06-14 18:30:07 -0400
     release

---
+++ lib/Mail/Audit/Vacation.pm
@@ -0,0 +1,58 @@
+package Mail::Audit::Vacation;
+use Mail::Audit;
+use vars qw(@VERSION $vacfile $message $subject $replyto $from);
+$VERSION = '1.0';
+$vacfile = ".vacation-cache";
+$message = "This user is on vacation.";
+$subject = "Vacation autoresponse";
+$replyto = $from = "<>";
+1;
+
+=head1 NAME
+
+Mail::Audit::Vacation - perform vacation autoresponding
+
+=cut
+
+package Mail::Audit;
+use strict;
+
+sub vacation {
+  my $item  = shift;
+  my $where = shift;
+  my $reply = $item->head->get("Reply-To")
+    || $item->head->get("From");
+  return if $item->head->get("Distribution") =~ /bulk/i or !$reply;
+  $item->_log(1, "Vacation thing from $reply");
+  if (open TOLD, $Mail::Audit::Vacation::vacfile) {
+    while (<TOLD>) {
+      if ($_ eq $reply) {
+        $item->accept($where);
+        return 1;  # Just in case.
+      }
+    }
+  }
+  if (open TOLD, ">>" . $Mail::Audit::Vacation::vacfile) {
+    print TOLD $item->head->get("Reply-To")
+      if $item->head->get("Reply-To");
+    print TOLD $item->head->get("From")
+      if $item->head->get("From");
+    close TOLD;
+    use Mail::Mailer qw(sendmail);
+    my $out = new Mail::Mailer;
+    $out->open(
+      {
+        From       => $Mail::Audit::Vacation::from,
+        Subject    => $Mail::Audit::Vacation::subject,
+        To         => $reply,
+        "Reply-To" => $Mail::Audit::Vacation::replyto
+      }
+    );
+    print $out $Mail::Audit::Vacation::message;
+    $out->close;
+  }
+  $item->accept($where);
+
+  return 0;
+}
+
